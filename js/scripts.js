3c5d828627943bf12e02919eda3a0867b9dd8b04
Stefan <s.pavlovic.rs@gmail.com>
2022-04-14 02:04:51 +0200

    Add left elements, style them, remove useless code

--- js/scripts.js
+++ js/scripts.js
@@ -7,13 +7,6 @@ function Book(title, author, pages, read, leftAt = 0) {
     this.pages = pages;
     this.read = read;
     this.leftAt = leftAt
-    this.info = ()=>{
-        if (read){
-            return `${title} by ${author}, ${pages} pages, read.`;
-        } else {
-            return `${title} by ${author}, ${pages} pages, not read yet.`;
-        }
-    }
 }
   
 function addBookToLibrary(object) {
