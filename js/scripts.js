9fddbba38926385f0849d35bd48b646a36b16fbd
Stefan <s.pavlovic.rs@gmail.com>
2022-04-15 01:29:09 +0200

    Keep track of stats, disable left at unless cond

--- js/scripts.js
+++ js/scripts.js
@@ -43,10 +43,13 @@ submit.addEventListener('click', ()=>{
 function createItem(){
     let wrapper = document.createElement('div');
     wrapper.classList.add('wrapper');
+    
     let book = document.createElement('div');
     book.classList.add('book');
     wrapper.appendChild(book);
+
     for (let [keys, values] of Object.entries(myLibrary.at(-1))){
+        // if reading status isn't Reading skip Left At section
         if (keys == 'left at' && myLibrary.at(-1)['reading status'] != 'Reading'){
         }else{
             let div = document.createElement('div');
@@ -65,8 +68,6 @@ function createItem(){
             }
         }
     }
-    
-    console.log(myLibrary.indexOf(myLibrary.at(-1)))
 
     // create buttons
 
@@ -76,16 +77,98 @@ function createItem(){
         // removeBook();
         console.log('removeBook')
     })
+
     let edit = document.createElement('img');
     edit.setAttribute('src', './img/pencil.png');
     edit.addEventListener('click', () => {
         // editBook();
         console.log("editBook")
     })
+
     let imgDiv = document.createElement('div');
     imgDiv.appendChild(remove)
     imgDiv.appendChild(edit)
     wrapper.appendChild(imgDiv)
+
+    wrapper.setAttribute('data-pos', myLibrary.indexOf(myLibrary.at(-1)));
+    remove.setAttribute('data-pos', myLibrary.indexOf(myLibrary.at(-1)));
+    edit.setAttribute('data-pos', myLibrary.indexOf(myLibrary.at(-1)));
         
+    statistics()
+
     middle.appendChild(wrapper);
 }
+
+const bookFin = document.querySelector('.books-finished')
+const bookUnfin = document.querySelector('.books-unfinished')
+const bookNotStart = document.querySelector('.books-not-started')
+const totPages = document.querySelector('.pages-read-total')
+const favAuth = document.querySelector('.favorite-author')
+const pageMost = document.querySelector('.most-pages')
+const pageLeast = document.querySelector('.least-pages')
+
+function statistics(){
+    let finishedBooks=0
+    let unfinishedBooks=0
+    let notStartedBooks=0
+    let totalPagesRead=0
+    let favoriteAuthor={}
+    let mostPages={}
+    let leastPages={}
+
+    myLibrary.forEach(item => {
+        if(item['reading status']=='Finished'){
+            finishedBooks+=1;
+            totalPagesRead+=Number(item['number of pages']);
+        }
+        if(item['reading status']=='Reading'){
+            unfinishedBooks+=1;
+            totalPagesRead+=Number(item['left at']);
+        }
+        if(item['reading status']=='Plan to read'){
+            notStartedBooks+=1;
+        }
+
+        // Keeps track of how many authors books are added
+        if (item.author in favoriteAuthor){
+            favoriteAuthor[item.author]+=1;
+        } else {
+            favoriteAuthor[item.author]=1;
+        }
+
+        // Keeps track of which book and made by whom has most pages
+        if (item['number of pages'] in mostPages){
+            mostPages[item.title + ' by ' + item.author + ': ' + item['number of pages'] + ' pages'] = item['number of pages'];
+        } else {
+            mostPages[item.title + ' by ' + item.author + ': ' + item['number of pages'] + ' pages'] = item['number of pages'];
+        }
+
+        // Keeps track of which book and made by whom has least pages
+        if (item['number of pages'] in leastPages){
+            leastPages[item.title + ' by ' + item.author + ': ' + item['number of pages'] + ' pages'] = item['number of pages'];
+        } else {
+            leastPages[item.title + ' by ' + item.author + ': ' + item['number of pages'] + ' pages'] = item['number of pages'];
+        }
+        
+    })
+    bookFin.textContent = finishedBooks;
+    bookUnfin.textContent = unfinishedBooks;
+    bookNotStart.textContent = notStartedBooks;
+    totPages.textContent = totalPagesRead;
+    favAuth.textContent = Object.keys(favoriteAuthor).reduce((a, b) => favoriteAuthor[a] > favoriteAuthor[b] ? a : b);
+    pageMost.textContent = Object.keys(mostPages).reduce((a, b) => mostPages[a] < mostPages[b] ? a : b);
+    pageLeast.textContent = Object.keys(leastPages).reduce((a, b) => leastPages[a] > leastPages[b] ? a : b);
+    
+}
+
+const readingStatusEvent = document.querySelector('#reading-status')
+const leftAtTextField = document.querySelector('#left-at')
+leftAtTextField.disabled = true;
+readingStatusEvent.addEventListener('change', (event)=>{
+    if (event.target.value != 'Reading'){
+        leftAtTextField.value = '';
+        leftAtTextField.disabled = true;
+    } else {
+        leftAtTextField.disabled = false;
+    }
+})
