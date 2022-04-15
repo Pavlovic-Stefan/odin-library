4ee442ffc2052fda9f8a36c85a48a3adeb0729d1
Stefan <s.pavlovic.rs@gmail.com>
2022-04-15 22:10:22 +0200

    Add edit functionally to books

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
 let myLibrary = [];
+let editState=false;
+let position;
+let remove = document.createElement('img');
+remove.setAttribute('src', './img/close.png');
+remove.classList.add('remove-button');
+let edit = document.createElement('img');
+edit.setAttribute('src', './img/pencil.png');
+edit.classList.add('edit-button');
 
 function Book(title, author, pages, read, leftAt = 0, note='') {
     // the constructor...
@@ -49,7 +57,6 @@ numPages.addEventListener('change', () => {
 function validate(){
     if (!leftAtPage.disabled){
         if (leftAtPage.value=='' || leftAtPage.value == 0){
-            console.log('cancer', leftAtPage.value)
             readingStatus.value = 'Plan to read'
             leftAtTextField.value = '';
             leftAtStatus()
@@ -72,21 +79,106 @@ function validate(){
     }
 }
 
+function insertAfter(newNode, referenceNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
+
 
 const add = document.querySelector('.add');
 add.addEventListener('click', ()=>{
     if (validate()){
-        let title = bookTitle.value;
-        let author = bookAuthor.value;
-        let pages = numPages.value;
-        let read = readingStatus.value;
-        let leftAt = leftAtPage.value;
-        let note = notes.value;
+        if (!editState){
+            let title = bookTitle.value;
+            let author = bookAuthor.value;
+            let pages = numPages.value;
+            let read = readingStatus.value;
+            let leftAt = leftAtPage.value;
+            let note = notes.value;
 
 
-        let newBook = new Book(title, author, pages, read, leftAt, note);
-        addBookToLibrary(newBook);
-        createItem()
+            let newBook = new Book(title, author, pages, read, leftAt, note);
+            addBookToLibrary(newBook);
+            createItem()
+        } else {
+            let exists = false;
+            let testing = document.querySelectorAll(`[data-pos="${position}"]>.book>div>p`);
+            let textNote = document.querySelector(`[data-pos="${position}"]>.book>div>textarea`);
+
+            testing.forEach(element => {
+                if (element.classList == 'leftat'){
+                    exists = true;
+                }
+            })
+            if (exists && readingStatus.value != 'Reading'){
+                testing.forEach(element => {
+                    if (element.classList == 'leftat'){
+                        element.parentNode.remove();
+                        testing.forEach(element => {
+                            if (element.classList == 'readingstatus'){
+                                element.textContent = readingStatus.value;
+                                myLibrary[position]['reading status'] = readingStatus.value;
+                            }
+                        })
+                        
+                        myLibrary[position]['left at'] = leftAtPage.value;
+                        statistics();
+                        editState = false
+
+                    }
+                }) 
+            }
+            if (readingStatus.value == 'Reading' || testing.item(testing.length - 1).classList != 'readingstatus'){
+                let div = document.createElement('div')
+                let head = document.createElement('h4')
+                head.textContent = 'LEFT AT'
+                let newReading = document.createElement('p');
+                newReading.classList.add('leftat');
+                newReading.setAttribute('data-pos', position);
+                newReading.textContent = leftAtPage.value;
+                div.appendChild(head);
+                div.appendChild(newReading);
+                if (testing.item(testing.length - 1 ).parentNode.parentNode!=null){
+                    insertAfter(div, [...testing.item(testing.length - 1 ).parentNode.parentNode.childNodes][3]);
+                }
+                myLibrary[position]['reading status'] = readingStatus.value;
+                myLibrary[position]['left at'] = leftAtPage.value;
+                
+            }
+            testing.forEach(element => {
+                if (element.classList == 'title'){
+                    element.textContent = bookTitle.value;
+                    myLibrary[position].title = bookTitle.value;
+                }
+                if (element.classList == 'author'){
+                    element.textContent = bookAuthor.value;
+                    myLibrary[position].author = bookAuthor.value;
+                }
+                if (element.classList == 'numberofpages'){
+                    element.textContent = numPages.value;
+                    myLibrary[position]['number of pages'] = numPages.value;
+                }
+                if (element.classList == 'readingstatus'){
+                    element.textContent = readingStatus.value;
+                    myLibrary[position]['reading status'] = readingStatus.value;
+                }
+                if (element.classList == 'leftat'){
+                    element.textContent = leftAtPage.value;
+                    myLibrary[position]['left at'] = leftAtPage.value;
+                }
+                if (element.classList == 'leftat'){
+                    if (element.textContent == '')
+                    element.parentElement.remove();
+                }
+
+            })
+            textNote.value = notes.value;
+            myLibrary[position].notes = notes.value;
+            editState = false
+            add.textContent = 'ADD';
+            clear();
+            statistics();
+            leftAtStatus();
+        }
     }
 })
 
@@ -98,11 +190,14 @@ function createItem(){
     book.classList.add('book');
     wrapper.appendChild(book);
 
+    let zeroCount = 0;
     for (let [keys, values] of Object.entries(myLibrary.at(-1))){
         // if reading status isn't Reading skip Left At section
         if (keys == 'left at' && myLibrary.at(-1)['reading status'] != 'Reading'){
         }else{
             let div = document.createElement('div');
+            div.classList.add(zeroCount);
+            zeroCount++;
             book.appendChild(div);
             let h4 = document.createElement('h4');
             h4.textContent = keys.toUpperCase();
@@ -111,56 +206,76 @@ function createItem(){
                 let textArea = document.createElement('textarea');
                 textArea.setAttribute('cols', '25');
                 textArea.setAttribute('rows', '6');
+                textArea.classList.add(keys.split(' ').join(""));
                 textArea.readOnly=true;
                 textArea.textContent = values;
                 div.appendChild(textArea);
             } else {
                 let p = document.createElement('p');
                 p.textContent = values;
+                p.classList.add(keys.split(' ').join(""));
                 div.appendChild(p);
             }
         }
     }
+    
 
     // create buttons
 
-    let remove = document.createElement('img');
-    remove.setAttribute('src', './img/close.png');
-    remove.classList.add('remove-button');
+    // remove books
     remove.addEventListener('click', () => {
-        let wrappers = document.querySelectorAll('.wrapper');
-        wrappers.forEach(wrap => {
-            if(wrap.getAttribute('data-pos')==remove.getAttribute('data-pos')){
-                myLibrary.splice(wrap.getAttribute('data-pos'), 1);
-                wrap.remove();
-            }
-        })
-        wrappers = document.querySelectorAll('.wrapper');
-        let track = 0;
-        wrappers.forEach(wrap => {
-            wrap.setAttribute('data-pos', track)
-            track++
-        })
-        track = 0;
-        let removeButtons = document.querySelectorAll('.remove-button');
-        removeButtons.forEach(button => {
-            button.setAttribute('data-pos', track)
-            track++
-        })
-        track = 0;
-        let editButtons = document.querySelectorAll('.edit-button');
-        editButtons.forEach(button => {
-            button.setAttribute('data-pos', track)
-            track++
-        })
+        if (!editState){
+            let wrappers = document.querySelectorAll('.wrapper');
+            wrappers.forEach(wrap => {
+                if(wrap.getAttribute('data-pos')==remove.getAttribute('data-pos')){
+                    myLibrary.splice(wrap.getAttribute('data-pos'), 1);
+                    wrap.remove();
+                }
+            })
+            wrappers = document.querySelectorAll('.wrapper');
+            let track = 0;
+            wrappers.forEach(wrap => {
+                wrap.setAttribute('data-pos', track)
+                track++
+            })
+            track = 0;
+            let removeButtons = document.querySelectorAll('.remove-button');
+            removeButtons.forEach(button => {
+                button.setAttribute('data-pos', track)
+                track++
+            })
+            track = 0;
+            let editButtons = document.querySelectorAll('.edit-button');
+            editButtons.forEach(button => {
+                button.setAttribute('data-pos', track)
+                track++
+            })
+        }
     })
 
-    let edit = document.createElement('img');
-    edit.setAttribute('src', './img/pencil.png');
-    edit.classList.add('edit-button');
+
+    // edit books
+
     edit.addEventListener('click', () => {
-        // editBook();
-        console.log("editBook")
+        let wrappersEdit = document.querySelectorAll('.wrapper');
+        wrappersEdit.forEach(wrap => {
+            if(wrap.getAttribute('data-pos')==edit.getAttribute('data-pos')){
+                let info = myLibrary[(wrap.getAttribute('data-pos'))];
+
+                bookTitle.value = info.title;
+                bookAuthor.value = info.author;
+                numPages.value = info['number of pages'];
+                readingStatus.value = info['reading status'];
+                leftAtPage.value = info['left at'];
+                notes.value = info.notes;
+                if (info['reading status'] == 'Reading'){
+                    leftAtTextField.disabled = false;
+                }
+                editState = true;
+                position = wrap.getAttribute('data-pos');
+                add.textContent = 'EDIT';
+            }
+        })
     })
 
     let imgDiv = document.createElement('div');
@@ -236,7 +351,6 @@ function statistics(){
     favAuth.textContent = Object.keys(favoriteAuthor).reduce((a, b) => favoriteAuthor[a] > favoriteAuthor[b] ? a : b);
     pageMost.textContent = Object.keys(mostPages).reduce((a, b) => mostPages[a] > mostPages[b] ? a : b);
     pageLeast.textContent = Object.keys(leastPages).reduce((a, b) => leastPages[a] < leastPages[b] ? a : b);
-    console.log(leastPages)
     
 }
 
